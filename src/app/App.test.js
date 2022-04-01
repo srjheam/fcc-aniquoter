0d084c9c4c6f0e4bf04dd5f532eff1fb99c9d23b
Jheam Storch Ross <jheam.storch@hotmail.com>
2022-04-01 15:10:19 -0300

    Fix App.test

--- src/app/App.test.js
+++ src/app/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders new quote button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const getQuoteBtn = screen.getAllByText(/new quote/i)[0];
+  expect(getQuoteBtn).toBeInTheDocument();
 });
