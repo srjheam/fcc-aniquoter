86e87653b1756ef186f40ead3c05b21a615b48ca
Jheam Storch Ross <jheam.storch@hotmail.com>
2022-04-01 16:46:38 -0300

    Add placeholder while quote is loading

--- src/features/quote/Quote.js
+++ src/features/quote/Quote.js
@@ -3,6 +3,24 @@ import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 import './Quote.scss';
 
 function Quote(props) {
+  if (!props.quote) {
+    return (
+      <div className='Quote placeholder'>
+        <FontAwesomeIcon icon={solid('quote-left')} className='quotemark quotemark-left placeholder-item' />
+        <div className='quote'>
+          <div className='placeholder-item' style={{ width: "80%" }}></div>
+          <div className='placeholder-item' style={{ width: "85%" }}></div>
+          <div className='placeholder-item' style={{ width: "75%" }}></div>
+        </div>
+        <FontAwesomeIcon icon={solid('quote-right')} className='quotemark quotemark-right placeholder-item' />
+        <div className='quote-authorship'>
+          <div className='placeholder-item' style={{ width: "30%" }}></div>
+          <div className='placeholder-item' style={{ width: "40%" }}></div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className='Quote'>
       <FontAwesomeIcon icon={solid('quote-left')} className='quotemark quotemark-left' />
