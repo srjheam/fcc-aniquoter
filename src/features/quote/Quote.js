d7199c3a76cafa419f5642a68f0d8f0251bdaec3
Jheam Storch Ross <jheam.storch@hotmail.com>
2022-04-01 14:27:57 -0300

    Add style to Quote

--- src/features/quote/Quote.js
+++ src/features/quote/Quote.js
@@ -1,13 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
+import './Quote.scss';
+
 function Quote(props) {
   return (
     <div className='Quote'>
-      <span>“</span>
-      <p>{props.quote}</p>
-      <p>
+      <FontAwesomeIcon icon={solid('quote-left')} className='quotemark quotemark-left' />
+      <p className='quote'>{props.quote}</p>
+      <FontAwesomeIcon icon={solid('quote-right')} className='quotemark quotemark-right' />
+      <p className='quote-authorship'>
         ― <em>{props.character}</em><br />
         from {props.anime}
       </p>
-      <span>”</span>
     </div>
   );
 }
